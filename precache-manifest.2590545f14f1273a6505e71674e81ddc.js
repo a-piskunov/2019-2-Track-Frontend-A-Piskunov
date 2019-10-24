05d346f0f762f1a3d892e0f535f7aa6147ba2c3d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-24 06:38:12 +0000

    Deploy a-piskunov/2019-2-Track-Frontend-A-Piskunov to github.com/a-piskunov/2019-2-Track-Frontend-A-Piskunov.git:gh-pages

---
+++ precache-manifest.2590545f14f1273a6505e71674e81ddc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bcf8e067bf6aa30b1b8be643cff7afb",
+    "url": "/a-piskunov/2019-2-Track-Frontend-A-Piskunov/index.html"
+  },
+  {
+    "revision": "5b25601486484772ba75",
+    "url": "/a-piskunov/2019-2-Track-Frontend-A-Piskunov/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "57260fee9dbf7c598c01",
+    "url": "/a-piskunov/2019-2-Track-Frontend-A-Piskunov/static/js/2.33a810b8.chunk.js"
+  },
+  {
+    "revision": "5b25601486484772ba75",
+    "url": "/a-piskunov/2019-2-Track-Frontend-A-Piskunov/static/js/main.d68b6111.chunk.js"
+  },
+  {
+    "revision": "ed21da93ee306a79368f",
+    "url": "/a-piskunov/2019-2-Track-Frontend-A-Piskunov/static/js/runtime-main.88b45778.js"
+  },
+  {
+    "revision": "795fb555bea801821f51202dc10aaa78",
+    "url": "/a-piskunov/2019-2-Track-Frontend-A-Piskunov/static/media/logo.795fb555.svg"
+  }
+]);
